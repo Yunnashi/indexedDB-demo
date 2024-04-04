6d03ed2ba85690858f7b891e54724b36369af958
Yuri <134183050+Yunnashi@users.noreply.github.com>
2024-04-04 11:45:06 +0900

    backup indexedDB for PWA

--- sw.js
+++ sw.js
@@ -34,3 +34,41 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
+
+/***********************************************************************************
+ * Service Workerがメッセージを受信してデータをバックアップし、起動時にデータをリストアする
+ ***********************************************************************************/
+// Service Workerがメッセージを受信する
+self.addEventListener('message', function(event) {
+  if (event.data.command === 'backup') {
+    caches.open(CACHE_NAME).then(function(cache) {
+      cache.put('backup', new Response(JSON.stringify(event.data.data)));
+    });
+  }
+});
+
+// Service Workerがキャッシュからデータを読み取る
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match('backup').then(function(response) {
+        if (response) {
+          return response.json();
+        }
+        return null;
+      }).then(function(data) {
+        if (data) {
+          // Service WorkerからWebページにメッセージを送信する
+          clients.matchAll().then(function(clients) {
+            clients.forEach(function(client) {
+              client.postMessage({
+                command: 'restore',
+                data: data
+              });
+            });
+          });
+        }
+      });
+    })
+  );
+});
